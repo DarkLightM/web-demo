3cf9048407194f8f44ee4faffd0f46a1e8315beb
DarkLight <max.gubar@outlook.com>
2021-09-15 17:51:16 +0500

    Added bootstrap

--- demo-web/src/main/java/com/example/demoweb/model/Post.java
+++ demo-web/src/main/java/com/example/demoweb/model/Post.java
@@ -1,11 +1,15 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
     public Post(String text){
         this.text = text;
+        this.creationDate = new Date();
         this.likes = 0;
     }
 
@@ -15,5 +19,9 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
